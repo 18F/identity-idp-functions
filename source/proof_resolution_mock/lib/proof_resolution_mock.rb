650c379267aeeb67d45b58e790f9cc6012d9ae31
Mitchell Henke <mitchell.henke@gsa.gov>
2020-10-09 16:23:41 -0500

    add mock resolution proofer

---
+++ source/proof_resolution_mock/lib/proof_resolution_mock.rb
@@ -0,0 +1,99 @@
+require 'proofer'
+require 'faraday'
+require 'retries'
+require_relative 'resolution_mock_client'
+require_relative 'state_id_mock_client'
+
+module IdentityIdpFunctions
+  class ProofResolutionMock
+    def self.handle(event:, context:, &callback_block)
+      params = JSON.parse(event['body'], symbolize_names: true)
+      new(
+        idp_api_auth_token: ENV['IDP_API_AUTH_TOKEN'],
+        **params,
+      ).proof(&callback_block)
+    end
+
+    attr_reader :applicant_pii, :callback_url, :should_proof_state_id, :idp_api_auth_token
+
+    def initialize(applicant_pii:, callback_url:, should_proof_state_id:, idp_api_auth_token:)
+      @applicant_pii = applicant_pii
+      @callback_url = callback_url
+      @should_proof_state_id =should_proof_state_id
+      @idp_api_auth_token = idp_api_auth_token
+    end
+
+    def proof(&callback_block)
+      proofer_result = with_retries(**retry_options) do
+        resolution_mock_proofer.proof(applicant_pii)
+      end
+
+      result = proofer_result.to_h
+      result[:context] = { stages: [
+        resolution: IdentityIdpFunctions::ResolutionMockClient.vendor_name
+      ] }
+
+      result[:timed_out] = proofer_result.timed_out?
+      result[:exception] = proofer_result.exception.inspect if proofer_result.exception
+
+
+      if should_proof_state_id && result[:success]
+        proof_state_id(result)
+      end
+
+      callback_body = {
+        resolution_result: result,
+      }
+
+      if block_given?
+        yield callback_body
+      else
+        post_callback(callback_body: callback_body)
+      end
+    end
+
+    def proof_state_id(result)
+      result[:context][:stages].push(state_id: IdentityIdpFunctions::StateIdMockClient.vendor_name)
+
+      proofer_result = with_retries(**retry_options) do
+        state_id_mock_proofer.proof(applicant_pii)
+      end
+
+      result.merge(proofer_result.to_h) do |key, orig, current|
+        key == :messages ? orig + current : current
+      end
+
+      result[:timed_out] = proofer_result.timed_out?
+      result[:exception] = proofer_result.exception.inspect if proofer_result.exception
+
+      result
+    end
+
+    def post_callback(callback_body:)
+      with_retries(**retry_options) do
+        Faraday.post(
+          callback_url,
+          callback_body.to_json,
+          "X-API-AUTH-TOKEN" => idp_api_auth_token,
+          "Content-Type" => 'application/json',
+          "Accept" => 'application/json'
+        )
+      end
+    end
+
+    def resolution_mock_proofer
+      IdentityIdpFunctions::ResolutionMockClient.new
+    end
+
+    def state_id_mock_proofer
+      IdentityIdpFunctions::StateIdMockClient.new
+    end
+
+    def retry_options
+      {
+        max_tries: 3,
+        rescue: [Faraday::TimeoutError, Faraday::ConnectionFailed],
+      }
+    end
+  end
+end
---
+++ source/proof_resolution_mock/spec/proof_resolution_mock_spec.rb
@@ -0,0 +1,169 @@
+require 'securerandom'
+require 'identity-idp-functions/proof_resolution_mock'
+
+RSpec.describe IdentityIdpFunctions::ProofResolutionMock do
+  let(:idp_api_auth_token) { SecureRandom.hex }
+  let(:callback_url) { 'https://example.login.gov/api/callbacks/proof-resolution/:token' }
+  let(:ssn) { '123456789' }
+  let(:bad_ssn) { IdentityIdpFunctions::ResolutionMockClient::NO_CONTACT_SSN }
+  let(:applicant_pii) do
+    {
+      first_name: 'Johnny',
+      last_name: 'Appleseed',
+      uuid: SecureRandom.hex,
+      dob: '01/01/1970',
+      ssn: ssn,
+      zipcode: '53206',
+      phone: '18888675309',
+      state_id_number: '123456',
+      state_id_type: 'drivers_license',
+      state_id_jurisdiction: 'WI',
+    }
+  end
+
+  describe '.handle' do
+    before do
+      stub_const(
+        'ENV',
+        'IDP_API_AUTH_TOKEN' => idp_api_auth_token,
+      )
+
+      stub_request(:post, callback_url).
+        with(
+          headers: {
+            'Content-Type' => 'application/json',
+            'X-API-AUTH-TOKEN' => idp_api_auth_token,
+          },
+      ) do |request|
+            expect(JSON.parse(request.body, symbolize_names: true)).to eq(
+              resolution_result: {
+                exception: nil,
+                errors: {},
+                messages: [],
+                success: true,
+                timed_out: false,
+                context: { stages: [
+                  { resolution: 'ResolutionMock' },
+                  { state_id: 'StateIdMock' },
+                ]}
+              }
+            )
+          end
+    end
+
+    let(:body) do
+      {
+        callback_url: callback_url,
+        should_proof_state_id: true,
+        applicant_pii: applicant_pii,
+      }
+    end
+
+    it 'runs' do
+      IdentityIdpFunctions::ProofResolutionMock.handle(event: { 'body' => body.to_json }, context: nil)
+    end
+
+    context 'when called with a block' do
+      it 'gives the results to the block instead of posting to the callback URL' do
+        yielded_result = nil
+        IdentityIdpFunctions::ProofResolutionMock.handle(
+          event: { 'body' => body.to_json },
+          context: nil
+        ) do |result|
+          yielded_result = result
+        end
+
+        expect(yielded_result).to eq(
+          resolution_result: {
+            exception: nil,
+            errors: {},
+            messages: [],
+            success: true,
+            timed_out: false,
+            context: { stages: [
+              { resolution: 'ResolutionMock' },
+              { state_id: 'StateIdMock' },
+            ]}
+          }
+        )
+
+        expect(a_request(:post, callback_url)).to_not have_been_made
+      end
+    end
+  end
+
+  describe '#proof' do
+    let(:should_proof_state_id) { true }
+
+    subject(:function) do
+      IdentityIdpFunctions::ProofResolutionMock.new(
+        callback_url: callback_url,
+        applicant_pii: applicant_pii,
+        should_proof_state_id: should_proof_state_id,
+        idp_api_auth_token: idp_api_auth_token,
+      )
+    end
+
+    before do
+      stub_request(:post, callback_url).
+        with(headers: { 'X-API-AUTH-TOKEN' => idp_api_auth_token })
+    end
+
+    context 'with a successful response from the proofer' do
+      it 'posts back to the callback url' do
+        function.proof
+
+        expect(WebMock).to have_requested(:post, callback_url)
+      end
+    end
+
+    context 'does not call state id with an unsuccessful response from the proofer' do
+      let(:ssn) { bad_ssn }
+      it 'posts back to the callback url' do
+        function.proof
+        expect_any_instance_of(IdentityIdpFunctions::StateIdMockClient).not_to receive(:proof)
+
+        expect(WebMock).to have_requested(:post, callback_url)
+      end
+    end
+
+    context 'with a connection error talking to the proofer' do
+      before do
+        allow(IdentityIdpFunctions::ProofResolutionMock).to receive(:proof).
+          and_raise(Faraday::ConnectionFailed.new('error')).
+          and_raise(Faraday::ConnectionFailed.new('error')).
+          and_raise(Faraday::ConnectionFailed.new('error'))
+      end
+
+      it 'retries 3 times then errors' do
+        expect(WebMock).to_not have_requested(:post, callback_url)
+      end
+    end
+
+    context 'with a connection error posting to the callback url' do
+      before do
+        stub_request(:post, callback_url).
+          to_timeout.
+          to_timeout.
+          to_timeout
+      end
+
+      it 'retries 3 then errors' do
+        expect { function.proof }.to raise_error(Faraday::ConnectionFailed)
+
+        expect(a_request(:post, callback_url)).to have_been_made.times(3)
+      end
+    end
+
+    context 'no state_id proof' do
+      let(:should_proof_state_id) { false }
+
+      it 'does not call state_id proof if resolution proof is successful' do
+        expect_any_instance_of(IdentityIdpFunctions::StateIdMockClient).not_to receive(:proof)
+        function.proof
+
+        expect(WebMock).to have_requested(:post, callback_url)
+      end
+    end
+  end
+end
