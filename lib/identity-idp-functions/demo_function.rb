26f2c1203c0f578e8b4145da02a2348aecb962d8
Brian Crissup <brian@BigMac.local>
2020-10-29 16:56:28 -0400

    Add permissions to get SSM parameters Remove demo function Rename layer Fix egress rules in security group

--- lib/identity-idp-functions/demo_function.rb
+++
@@ -1,4 +0,0 @@
-require 'identity-idp-functions'
-
-# This is a trampoline to allow lazily loading the demo_function function
-require IdentityIdpFunctions.function_path('demo_function')
--- spec/identity-idp-functions_spec.rb
+++ spec/identity-idp-functions_spec.rb
@@ -2,9 +2,4 @@ RSpec.describe IdentityIdpFunctions do
   it "has a version number" do
     expect(IdentityIdpFunctions::VERSION).not_to be nil
   end
-
-  it "loads the handlers functions" do
-    require 'identity-idp-functions/demo_function'
-    expect(defined? IdentityIdpFunctions::DemoFunction).to be_truthy
-  end
 end
