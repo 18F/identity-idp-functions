48f3dd01bc1a7e12e52c76fa16ea71ad8327dff3
Mitchell Henke <mitchell.henke@gsa.gov>
2020-11-10 15:14:58 -0600

    rubocop

--- spec/shared_examples_for_proofers.rb
+++ spec/shared_examples_for_proofers.rb
@@ -6,7 +6,6 @@ RSpec.shared_examples 'misconfigured proofer' do
   end
 
   it 'raises error if auth token is not configured and block is not given' do
-
     expect { function.proof }.to raise_exception 'IDP_API_AUTH_TOKEN is not configured'
 
     expect(WebMock).to_not have_requested(:post, callback_url)
