482b294997b8d0e7bc5e11d54cd01008b2a256f9
Zach Margolis <zachary.margolis@gsa.gov>
2020-11-24 12:46:00 -0800

    Create S3Helper and EncryptionHelper

--- source/aws-ruby-sdk/function_helper.rb
+++ source/aws-ruby-sdk/function_helper.rb
@@ -1,7 +1,9 @@
+require_relative './encryption_helper'
 require_relative './errors'
 require_relative './faraday_helper'
 require_relative './logging_helper'
 require_relative './ssm_helper'
+require_relative './s3_helper'
 require_relative './timer'
 
 module IdentityIdpFunctions
