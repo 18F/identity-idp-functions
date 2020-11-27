642aa8b19897fbed4374a7db269beee3c7197c7f
Zach Margolis <zachary.margolis@gsa.gov>
2020-11-27 09:47:36 -0800

    Move encrypt method onto EncryptionHelper so we can use it downstream

--- source/aws-ruby-sdk/encryption_helper.rb
+++ source/aws-ruby-sdk/encryption_helper.rb
@@ -12,5 +12,18 @@ module IdentityIdpFunctions
 
       cipher.update(data[0..-17]) + cipher.final
     end
+
+    def encrypt(data:, iv:, key:)
+      cipher = OpenSSL::Cipher.new('aes-256-gcm')
+      cipher.encrypt
+      cipher.iv = iv
+      cipher.key = key
+      cipher.auth_data = ''
+
+      encrypted = cipher.update(data) + cipher.final
+      tag = cipher.auth_tag # produces 16 bytes tag by default
+
+      encrypted + tag
+    end
   end
 end
